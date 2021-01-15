1c48132f36b6bdab2767b73deb47e9eca0a41641
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-15 18:04:04 +0600

    doubly linked list insert-delete created

--- List/Doubly Link List/Doubly linked list insert.cpp
+++ List/Doubly Link List/Doubly linked list insert.cpp
@@ -1,1 +1,30 @@
-
+template<typename T>
+void DoublyLinkedList<T>::insert(T data,int index)
+{
+    if(index<0)
+    {
+        cout<<"index<0"<<endl;
+        return ;
+    }
+    int temp;
+    Node<T>*pNode=header;
+    while(temp<index && pNode->next != nullptr)
+    {
+        pNode=pNode->next;
+        temp++;
+    }
+    if(pNode==nullptr)
+    {
+        cout<<"index out of bounds"<<endl;
+        return;
+    }
+    Node<T>*s=new Node<T>();
+    s->data=data;
+    s->prev=pNode;
+    s->next=pNode->next;
+    if(pNode->next !=nullptr)
+    {
+        pNode->next->prev=s;
+    }
+    pNode->next=s;
+}
