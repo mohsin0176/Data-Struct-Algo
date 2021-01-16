958ec3693c994b7b57862041c1f57e0b18e6351d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-16 17:29:25 +0600

    sort created

--- Sort/bubble.cpp
+++ Sort/bubble.cpp
@@ -1,1 +1,13 @@
-
+void bubbleSort(int src[],int length)
+{
+    for(int i=0;i<length;i++)
+    {
+        for(int j=length-1;j>i;j--)
+        {
+            if(src[j]<src[j-1])
+            {
+                swap(src,j,j-1);
+            }
+        }
+    }
+}
