71fb516c4a28beb5a4faf11e1e3dd037cb992c71
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-16 17:44:39 +0600

    sort created

--- Sort/select.cpp
+++ Sort/select.cpp
@@ -1,1 +1,19 @@
 
+void selectionSort(int src[],int length)
+{
+    for(int i=0;i<length;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<length;j++)
+        {
+            if(src[min]>src[j])
+            {
+                min=j;
+            }
+        }
+        if(min !=i)
+        {
+            swap(src,i,min);
+        }
+    }
+}
--- Sort/shell.cpp
+++ Sort/shell.cpp
@@ -1,1 +1,21 @@
-
+void shellSort(int src[],int length)
+{
+    int step=length/2;
+    while(step!=0)
+    {
+        for(int i=0;i<step;i++)
+        {
+            for(int j=i+step;j<length;j+=step)
+            {
+                int temp=src[j];
+                int k;
+                for(k=j-step;k>=0&&src[k]>temp;k-=step)
+                {
+                    src[k+step]=src[k];
+                }
+                src[k+step]=temp;
+            }
+        }
+        step=step/2;
+    }
+}
