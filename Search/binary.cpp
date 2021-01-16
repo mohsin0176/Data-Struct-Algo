69bee7a5429b43003f0f179d41173fba998f64ac
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-16 17:23:10 +0600

    search created

--- Search/binary.cpp
+++ Search/binary.cpp
@@ -1,1 +1,26 @@
 
+int binarySearch(int src[],int length,int key)
+{
+    int low,mid,high;
+    low=0;
+    high=lenght-1;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(key<src[mid])
+        {
+            high=mid-1;
+        }
+        else if(key>src[mid])
+        {
+            low=mid+1;
+        }
+        else
+        {
+            cout<<"Key Found";
+            return mid;
+        }
+    }
+    cout<<"Not Found key"<<endl;
+    return 0;
+}
--- Search/interpolation.cpp
+++ Search/interpolation.cpp
@@ -1,1 +1,26 @@
 
+int interpolationSearch(int src[],int length,int key)
+{
+    int low,mid,high;
+    low=0;
+    high=lenght-1;
+    while(low<=high)
+    {
+        mid=low+(high-low)*(key-src[low])/(src[high]-src[low]);
+        if(key<src[mid])
+        {
+            high=mid-1;
+        }
+        else if(key>src[mid])
+        {
+            low=mid+1;
+        }
+        else
+        {
+            cout<<"Key Found";
+            return mid;
+        }
+    }
+    cout<<"Not Found key"<<endl;
+    return 0;
+}
