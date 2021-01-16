1c3e4f6a5a1b2a31c304faa1a7494bfb79474020
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-16 18:07:02 +0600

    sort created

--- Sort/merge.cpp
+++ Sort/merge.cpp
@@ -1,1 +1,54 @@
 
+void merge(int src[],int length,int start,int mid,int end)
+{
+    int temp[length];
+    int left=start;
+    int tmpIndex=start;
+    int index=start;
+    while(left<=mid&&right<=end)
+    {
+        if(src[left]<=src[right])
+        {
+            temp[tmpIndex++]=src[left++];
+        }
+        else
+        {
+            temp[tmpIndex++]=src[right++];
+        }
+    }
+    while(left<=mid)
+    {
+        temp[tmpIndex++]=src[left++];
+    }
+    while(right<=end)
+    {
+        temp[tmpIndex++]=src[right++];
+    }
+    while(index<=end)
+    {
+        src[index]=temp[index];
+        index++;
+    }
+}
+void mergeSort(int src[],int length)
+{
+    int len=1;
+    int low=0;
+    int mid;
+    int high;
+    while(len<=length)
+    {
+        for(int i=0;i+len<=length-1;i+=len*2)
+        {
+            low=i;
+            mid=i+len-1;
+            high=i+len*2-1;
+            if(high>length-1)
+            {
+                high=length-1;
+            }
+            merge(src,length,low,mid,high);
+        }
+        len=2*len;
+    }
+}
--- Sort/quick.cpp
+++ Sort/quick.cpp
@@ -1,1 +1,33 @@
+int partArr(int src[],int low,int high)
+{
+    int pivot=src[low];
+    while(low<high)
+    {
+        while(low<high&&src[high]>=pivot)
+        {
+            high--;
+        }
+        swap(src,low,high);
+        while(low<high&&src[low]<=pivot)
+        {
+            low++;
+        }
+        swap(src,low,high);
+    }
+    return low;
+}
+void qSort(int src[],int low,int high)
+{
+    int pivot;
+    if(low<=high)
+    {
+        pivot=partArr(src,low,high);
+        qSort(src,low,pivot-1);
+        qSort(src,pivot+1,high);
 
+    }
+}
+void quickSort(int src[],int length)
+{
+    qSort(src,0,length-1);
+}
